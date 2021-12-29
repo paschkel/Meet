f6c26fcdb38431311e6a284d52848c06d2863f77
Pascal <95686230+paschkel@users.noreply.github.com>
2021-12-29 18:02:24 +0100

    Add files via upload
    IR Controller Initial File

---
+++ irController.h
@@ -0,0 +1,24 @@
+
+// Format
+#define		INIT_IR_FRAME_FORMAT	system("sudo ir-keytable -p nec")			// Set NEC
+
+
+// Keys
+#define 	ON_OFF		0x0c
+#define 	GREEN		0x45
+#define 	RED			0x47
+
+#define 	MUTE		0x46
+#define 	VIDEO		0x5e
+#define 	VIEW		0x0d
+
+#define 	FAV_1		0x44
+#define 	FAV_2		0x43
+#define 	FAV_3		0x07
+
+#define 	FAV_4		0x09
+#define 	FAV_5		0x16
+
+#define 	PLUS		0x5a
+#define 	MINUS		0x08
+
