9621cda31b788d6b2a30ecaca392e926bef4ff29
Pascal <95686230+paschkel@users.noreply.github.com>
2022-02-04 07:05:07 +0100

    Add files via upload

---
+++ scr/irController2.h
@@ -0,0 +1,25 @@
+
+// Format
+#define		INIT_IR_FRAME_FORMAT	"nec"			// Set NEC
+
+#define		INIT_IR_EVENT_TYPE		4
+#define		INIT_IR_EVENT_CODE		4
+// Keys
+#define 	ON_OFF		0x10
+#define 	GREEN		0x1f
+#define 	RED			0x0d
+
+#define 	MUTE		0x09
+#define 	VIDEO		0x1d
+#define 	VIEW		0x19
+
+#define 	FAV_1		0x17
+#define 	FAV_2		0x40
+#define 	FAV_3		0x0a
+
+#define 	FAV_4		0x1c
+#define 	FAV_5		0x12
+
+#define 	PLUS		0x15
+#define 	MINUS		0x11
+
